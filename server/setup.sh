0a1f7f795eb6359c75cb8468bcce480c02a86288
Devin <104116486+DMuld@users.noreply.github.com>
2025-03-14 13:09:22 -0500

    Update setup.sh

--- server/setup.sh
+++ server/setup.sh
@@ -3,6 +3,3 @@
 source .env
 
 sudo docker run -it -p 7777:7777 --rm -v ${PROJECT_DIR}/terraria:/root/.local/share/Terraria/Worlds ryshe/terraria:latest -world /root/.local/share/Terraria/Worlds/world.wld -autocreate 3
-
-docker compose up -d
-
