a1887f24c63da3d4cf3a63173b65f5f4ce169920
DMuld <mulder.z.devin@gmail.com>
2025-07-26 20:07:22 -0500

    Added webserver for self hosting website

--- server/setup.sh
+++ server/setup.sh
@@ -2,4 +2,9 @@
 
 source .env
 
-sudo docker run -it -p 7777:7777 --rm -v ${PROJECT_DIR}/terraria:/root/.local/share/Terraria/Worlds ryshe/terraria:latest -world /root/.local/share/Terraria/Worlds/world.wld -autocreate 3
+# sudo docker run -it -p 7777:7777 --rm -v ${PROJECT_DIR}/terraria:/root/.local/share/Terraria/Worlds ryshe/terraria:latest -world /root/.local/share/Terraria/Worlds/world.wld -autocreate 3
+
+cd meme-drop/
+sudo docker compose --build 
+cd ../
+sudo docker compose up
